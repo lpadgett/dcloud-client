2c60d7425a0f58cbdf9c412b8463b798b23caa93
Luke Padgett <lukepadgett@Lukes-MacBook-Pro.local>
2020-05-07 23:17:26 -0500

    Cosmetic fixing, work on Vuex later. Check out branch from here to work on further cosmetic stuff and adding components to homepage

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,28 +1,42 @@
 <template>
   <v-app>
-    <div>
-    <v-app-bar app="false" color="primary"> <!-- Turn app bar into component --->
-        <LogoSVG height=90 width="90"/>
-      <v-spacer/>
-      <div id="appbarButtons">
-        <v-btn color=""></v-btn>
-      </div>
-    </v-app-bar>
+      <div>
+        <v-app-bar app="false" color="primary"> <!-- Turn app bar into component --->
+          <div id="logo" class="pa-2">
+            <LogoSVG height=90 width="90"/>
+          </div>
+
+          <div id="pageTitle">
+            <h1>DCloud</h1>
+          </div>
+
+          <v-spacer/>
+
+          <div id="appbarButton1" class="pa-1">
+            <v-btn color="secondary" depressed >Sign In To DCloud</v-btn>
+          </div>
+
+          <div id="appbarButton2" class="pa-5">
+            <v-btn color="secondary" depressed fab></v-btn>
+          </div>
+        </v-app-bar>
       </div>
     <v-content>
-      <ContentStuff/>
+      <Home/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import LogoSVG from './LogoSVG';
+import Home from './Home';
 
 export default {
   name: 'DCloud',
 
   components: {
     LogoSVG,
+    Home
   },
 
   data: () => ({
@@ -30,3 +44,9 @@ export default {
   }),
 };
 </script>
+
+<style>
+  h1 {
+    color: white;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,4 @@
 <template>
+    <p>Test</p>
 </template>
 
