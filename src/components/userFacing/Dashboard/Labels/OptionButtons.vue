36a050bd14b39e7bbb46e785d59ff471401fcc99
Luke Padgett <lukepadgett@unknown020fb5f13f6c.home>
2020-05-23 17:04:15 -0500

    Needed v-tooltip for OptionButtons, not v-hover

--- src/components/userFacing/Dashboard/Labels/OptionButtons.vue
+++ src/components/userFacing/Dashboard/Labels/OptionButtons.vue
@@ -1,10 +1,15 @@
 <template>
-    <v-hover>
-        <v-btn text>button1</v-btn>
+    <div>
+        <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+                <v-btn text v-on="on">button1</v-btn>
+            </template>
+            <span>Tooltip</span>
+        </v-tooltip>
         <v-btn text>button2</v-btn>
         <v-btn text>button3</v-btn>
         <v-btn text>button4</v-btn>
-    </v-hover>
+    </div>
 </template>
 
 <script>
