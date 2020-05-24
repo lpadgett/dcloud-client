0db42a164ab982c8e3554db0aba973de02499eb5
Luke Padgett <lukepadgett@Lukes-MBP.home>
2020-05-24 00:27:42 -0500

    Redesigned file preview function

--- src/components/userFacing/Dashboard/FilesList/File.vue
+++ src/components/userFacing/Dashboard/FilesList/File.vue
@@ -4,7 +4,7 @@
                 v-for="file in files"
                 :key="file.name"
                 link
-                v-on:click="showPreview(file.source)"
+                v-on:click="clickFile(file)"
         >
             <!--state changes for click belong in list item-->
             <!--v-on:click=""-->
@@ -38,8 +38,10 @@
             },
         },
         methods: {
-            showPreview: function(sourceLink) {
-                window.open(sourceLink, "_blank");
+            clickFile: function(file) {
+                if(file.type !== 'folder') {
+                    window.open(file.source, "_blank");
+                }
             }
         },
     }
