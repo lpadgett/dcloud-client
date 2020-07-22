f86496d48a5e27ab42a617608eac3e490d90d2b2
Luke Padgett <lukepadgett@Lukes-MacBook-Pro.local>
2020-07-21 22:53:21 -0500

    Add filepath history to breadcrumb menu objects

--- src/store/modules/UserFiles/userFiles.module.js
+++ src/store/modules/UserFiles/userFiles.module.js
@@ -250,7 +250,137 @@ const state = {
         },
     ],
     filepathStack: [],
-    folderHistory: [],
+    folderHistory: [
+        {
+            text: "My DCloud",
+            disabled: false, //Add href to make clickable
+            linkedFiles: [
+                {
+                    parent: null,
+                    id: "1a",
+                    name: "testFolder1",
+                    type: "folder",
+                    size: "-",
+                    source: null,
+                    lastModified: "date1",
+                    favorite: true,
+                    trashed: false,
+                    children: [
+                        {
+                            parent: "1a",
+                            id: "1b",
+                            name: "test1",
+                            type: "picture",
+                            size: "1kb",
+                            source: "https://siasky.net/CAATt9wrsMQ47g-18XZYhXTwIVoFNlz9j8iKnIeJg2kXhA",
+                            lastModified: "date2",
+                            favorite: false,
+                            trashed: false,
+                            children: null
+                        },
+                        {
+                            parent: "1a",
+                            id: "1c",
+                            name: "Doesnt Exist Yet",
+                            type: "picture",
+                            size: "1kb",
+                            source: null,
+                            lastModified: "date3",
+                            favorite: false,
+                            trashed: false,
+                            children: null
+                        },
+                        {
+                            parent: "1a",
+                            id: "1d",
+                            name: "secondLayerForFileNavbarTest",
+                            type: "folder",
+                            side: "-",
+                            source: null,
+                            lastModified: "date test",
+                            favorite: false,
+                            trashed: false,
+                            children: [
+                                {
+                                    parent: "1a",
+                                    id: "1e",
+                                    name: "thirdLayerForFileNavbarTest",
+                                    type: "folder",
+                                    size: "-",
+                                    source: null,
+                                    lastModified: "date test1",
+                                    favorite: false,
+                                    trashed: false,
+                                    children: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    parent: null,
+                    id: "2a",
+                    name: "testFolder2",
+                    type: "folder",
+                    size: "-",
+                    source: null,
+                    lastModified: "date4",
+                    favorite: false,
+                    trashed: false,
+                    children: [
+                        {
+                            parent: "2a",
+                            id: "2b",
+                            name: "test2",
+                            type: "picture",
+                            size: "1kb",
+                            source: "https://siasky.net/EACXV8iHR_k5m9bIlNP1MbQzNzJoNLnIJpdgNxKt7P1_ig",
+                            lastModified: "date5",
+                            favorite: false,
+                            trashed: false,
+                            children: null
+                        },
+                        {
+                            parent: "2a",
+                            id: "2c",
+                            name: "This doesnt exist yet either",
+                            type: "picture",
+                            size: "1kb",
+                            source: null,
+                            lastModified: "date6",
+                            favorite: false,
+                            trashed: false,
+                            children: null
+                        }
+                    ]
+                },
+                {
+                    parent: null,
+                    id: "1c",
+                    name: "testFolder3",
+                    type: "folder",
+                    size: "-",
+                    source: null,
+                    lastModified: "date7",
+                    favorite: true,
+                    trashed: false,
+                    children: []
+                },
+                {
+                    parent: null,
+                    id: "2b",
+                    name: "test3",
+                    type: "picture",
+                    size: "1kb",
+                    source: "https://siasky.net/CADdUqC4NLdlS79eyjk5DDINntKWm3B1cRjKPTnXszldLw",
+                    lastModified: "date1",
+                    favorite: false,
+                    trashed: false,
+                    children: null
+                },
+            ]
+        }
+    ],
     isLoading: false
 };
 
@@ -286,6 +416,7 @@ const mutations = {
             {
                 text: "My DCloud",
                 disabled: false, //Add href to make clickable
+                linkedFiles: state.userFiles
             }
         ];
     },
@@ -293,11 +424,12 @@ const mutations = {
         if(file.type !== 'folder' && file.source !== null) {
             window.open(file.source, "_blank");
         } else if(file.children !== null) {
-            state.filepathStack.push(state.currentFilesBeingViewed);
+            state.filepathStack.push(state.currentFilesBeingViewed); //TODO: Get rid of redundant filepathstack state var
             state.folderHistory.push(
                 {
                     text: file.name,
                     disabled: false, //Add href to make clickable
+                    linkedFiles: state.currentFilesBeingViewed //Store filepath stack inside of folder history for easy nav
                 }
             )
             state.currentFilesBeingViewed = file.children;
